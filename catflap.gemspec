09569b216c0aa9455a747d66d64d79dba07f8fdb
Nicholas Cowham <Nicholas.Cowham@corbis.com>
2016-01-30 15:28:59 +0700

    Abstracted iptables into a plugin architecture to allow support for other firewall systems like ufw, ipfw, etc.

--- catflap.gemspec
+++ catflap.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name        = 'catflap'
-  s.version     = '0.0.2'
+  s.version     = '1.0.0'
   s.summary     = 'Manage NetFilter-based rules to grant port access on-demand via commandline or REST API requests.'
   s.description = 'A simple solution to provide on-demand service access (e.g. port 80 on webserver), where a more robust and secure VPN solution is not available.'
   s.authors     = ['Nyk Cowham']
-  s.email       = 'nyk@demotix.com'
-  s.files 	= ['lib/catflap.rb', 'lib/catflap-http.rb', 'bin/catflap']
+  s.email       = 'nykcowham@gmail.com'
+  s.files 	= ['lib/catflap.rb', 'lib/catflap-http.rb', 'bin/catflap', 'lib/plugins/firewall/iptables.rb']
   s.executables = ['catflap']
   s.licenses	= ['MIT']
   s.requirements = 'NetFilters (iptables) installed and working.'
--- lib/catflap.rb
+++ lib/catflap.rb
@@ -2,71 +2,72 @@ require 'yaml'
 require 'ipaddr'
 
 class Catflap
+  Version = '1.0.0.pre'
 
-  attr_accessor :config, :chain, :port, :dports, :print, :noop, :log_rejected
+  attr_accessor :print, :noop, :log_rejected
+  attr_reader :fwplugin, :port, :docroot, :dports, :chain
 
   def initialize config_file
     config_file = config_file || '/usr/local/etc/catflap.yaml'
     @config = {}
     @config = YAML.load_file config_file if File.readable? config_file
     @port = @config['server']['port'] || 4777
-    @chain = @config['rules']['chain'] || 'catflap-accept'
-    @dports = @config['rules']['dports'] || '80,443'
+    @docroot = @config['server']['docroot'] || './htdoc'
+    @fwplugin = @config['firewall']['plugin'] || 'iptables'
+    @chain = @config['firewall']['chain'] || 'catflap-accept'
+    @dports = @config['firewall']['dports'] || '80,443'
     @print = false
     @noop = false
     @log_rejected = true
+    initialize_firewall_plugin
+  end
+
+  def initialize_firewall_plugin
+    require_relative "plugins/firewall/#{@fwplugin}.rb"
+    @firewall = Object.const_get(@fwplugin.capitalize).new @config['firewall']
+  end
+
+  def print_version
+    puts "Catflap version #{Version}"
   end
 
   def install_rules!
-    output  = "iptables -N #{@chain}\n" # Create a new user-defined chain as a container for our catflap netfilter rules
-    output << "iptables -A #{@chain} -s 127.0.0.1 -p tcp -m multiport --dports #{@dports} -j ACCEPT\n" # Accept packets to localhost
-    output << "iptables -A INPUT -p tcp -m multiport --dports #{@dports} -j #{@chain}\n" # Jump from INPUT chain to the catflap chain
-    output << "iptables -A INPUT -p tcp -m multiport --dports #{@dports} -j LOG\n" if @log_rejected # Log any rejected packets to /var/log/messages
-    output << "iptables -A INPUT -p tcp -m multiport --dports #{@dports} -j DROP\n" # Drop any other packets to the ports on the INPUT chain
-    execute! output
+    execute! @firewall.install_rules
   end
 
   def uninstall_rules!
-    output  = "iptables -D INPUT -p tcp -m multiport --dports #{@dports} -j #{@chain}\n" # Remove user-defined chain from INPUT chain
-    output << "iptables -F #{@chain}\n" # Flush the catflap user-defined chain
-    output << "iptables -X #{@chain}\n" # Remove the catflap chain
-    output << "iptables -D INPUT -p tcp -m multiport --dports #{@dports} -j LOG\n" # Remove the logging rule
-    output << "iptables -D INPUT -p tcp -m multiport --dports #{@dports} -j DROP\n" # Remove the packet dropping rule
-    execute! output
+    execute! @firewall.uninstall_rules
   end
 
   def purge_rules!
-    output = "iptables -F #{@chain}"
-    execute! output
+    execute! @firewall.purge_rules
   end
 
   def list_rules
-    system "iptables -S #{@chain}"
+    system @firewall.list_rules
   end
 
   def check_address ip
     check_user_input ip
-    return system "iptables -C #{@chain} -s #{ip} -p tcp -m multiport --dports #{@dports} -j ACCEPT\n"
+    return system @firewall.check_address ip
   end
 
   def add_address! ip
     check_user_input ip
-    output = "iptables -I #{@chain} 1 -s #{ip} -p tcp -m multiport --dports #{@dports} -j ACCEPT\n"
-    execute! output
+    execute! @firewall.add_address ip
   end
 
   def delete_address! ip
     check_user_input ip
-    output = "iptables -D #{@chain} -s #{ip} -p tcp -m multiport --dports #{@dports} -j ACCEPT\n"
-    execute! output
-  end 
+    execute! @firewall.delete_address ip
+  end
 
   def add_addresses_from_file! filepath
     if File.readable? filepath
       output = ""
       File.open(filepath, "r").each_line do |ip|
         check_user_input ip
-        output << "iptables -I #{@chain} 1 -s #{ip.chomp} -p tcp -m multiport --dports #{@dports} -j ACCEPT\n"
+        output += @firewall.add_address ip.chomp
       end
       execute! output
     else
@@ -78,7 +79,7 @@ class Catflap
   def execute! output
     if @print then puts output end
     system output unless @noop
-  end 
+  end
 
   def check_user_input suspect
     return IPAddr.new(suspect)
